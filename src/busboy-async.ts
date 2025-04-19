e4d94853aaa1d59c68fbe30cf8a2215dabed2d1b
gh-andre <gh-andre@users.noreply.github.com>
2025-04-19 15:48:01 -0400

    Reworked error processing

--- src/busboy-async.ts
+++ src/busboy-async.ts
@@ -34,6 +34,8 @@ export type BusboyAsyncEvent = FieldEvent|FileEvent|LimitEvent;
 export class BusboyAsync {
         private bb: busboy.Busboy;
 
+        private bbError?: Error;
+
         constructor (busboyConfig: busboy.BusboyConfig) 
         {
             this.bb = busboy(busboyConfig);
@@ -93,60 +95,67 @@ export class BusboyAsync {
                 bbEnd = true;
             });
 
-            while(!bbEnd)
+            while(!bbEnd && !this.bbError) {
                 // yield the next event if there is one, and otherwise allow streams to pump more data
                 if(bbEvents.length == 0)
                     await new Promise<void>((io_done) => {setImmediate(() => {io_done();});});
                 else
                     yield bbEvents.shift() as FileEvent|FieldEvent|LimitEvent;
+            }
+
+            // throw the original error, which contains the call stack captured in the data pump loop
+            if(this.bbError)
+                throw this.bbError;
         }
 
-        feedBusboy(inStream: stream.Readable) : Promise<void|Error>
+        feedBusboy(inStream: stream.Readable, catchHandler?: (error: any) => any|Promise<any>) : Promise<void|Error>
         {
-            return new Promise<void|Error>((bbDone: () => void, bbError: (reason: any) => void) : void => {
+            let bbDataFeed: Promise<void|Error> = new Promise<void|Error>((bbDone: () => void, bbError: (reason: any) => void) : void => {
                 inStream
                     .on("error", (error: Error) => {
-                        bbError(error);
-
-                        // if streams are not destroyed, Readable.pipe stops pumping data and "finish" will not be emitted
-                        if(!inStream.destroyed)
-                            inStream.destroy();
-
+                        //
+                        // There appears to be a design flaw in busboy in which an error
+                        // in the form data stream will not propagate into the secondary
+                        // file stream created by busboy when encountering file data,
+                        // which results in the disconnected primary pipe that will never
+                        // supply more data and the secondary file stream waiting for the
+                        // data forever. This can be reproduced by closing the browser
+                        // after the file even was emitted by the primary parsing stream.
+                        // As a workaround destroy explicitly the busboy stream, which
+                        // propagates to the file stream, and terminates the file upload.
+                        //
                         if(!this.bb.destroyed)
                             this.bb.destroy(error);
-                    })
-                    .on("finish", () => {
-                        bbDone();
                     });
 
-                inStream.pipe(this.bb)
+                this.bb
                     .on("error", (error: Error) => {
                         bbError(error);
-
-                        if(!this.bb.destroyed)
-                            this.bb.destroy();
-
-                        if(!inStream.destroyed)
-                            inStream.destroy(error);
                     })
                     .on("finish", () => {
                         bbDone();
                     });
 
-                inStream.resume();
-            })
-            //
-            // The returned promise will not be await'ed immediately after
-            // it is returned, which will likely cause an unhandled promise
-            // rejection exception if there is some intermediate `await` for
-            // an unrelated promise, such as a file read (i.e. a promise must
-            // be handled within a single event loop iteration). In most cases
-            // this error will be the same as is the one that ended the data
-            // pump above, but for debugging purposes callers may inspect the
-            // awaited value of this promise to check if it's something else.
-            //
-            .catch((error?: any) : any => {
-                return Promise.resolve(!error ? new Error("Unknown error in feedBusboy") : error);
+                inStream.pipe(this.bb);
             });
+
+            //
+            // The returned promise may not be await'ed immediately after
+            // it's returned, which may cause an unhandled promise rejection
+            // if there's some intermediate `await` for an unrelated promise,
+            // such as a file read (i.e. if a promise is not handled within
+            // a single event loop iteration it is considered as unhandled
+            // rejection). In most cases the error captured here will be the
+            // same as the one that ended the data pump above, but for
+            // debugging purposes callers that didn't provide their own
+            // catch handler may inspect the awaited value of this promise
+            // to see if it's something else.
+            //
+            if(catchHandler != undefined)
+                bbDataFeed.catch((error: any) : any|Promise<any> => {this.bbError = error; catchHandler(error);});
+            else
+                bbDataFeed.catch((error: any) : any|Promise<any> => {this.bbError = error; return Promise.resolve(error);});
+
+            return bbDataFeed;
         }
 }
